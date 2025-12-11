236db4afdf1d50515f774249e03be23c7595c96c
Mexaniker <smtshag@gmail.com>
2025-12-11 17:07:49 +0300

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Enjoy/docs/assets/index-jXSuJ3Jl.js',  // имя репозитория!
   plugins: [react()],
-})
+});
